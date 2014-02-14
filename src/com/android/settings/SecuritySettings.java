f25f4aae901f574bc92da235d946b0c3127495c7
lichti <lichti@lichti>
2014-02-14 21:55:14 +0100

    Revert "Revert "bring back app opps in settings, place privacy guard and app opps directly under build tweaks and delate privacy guard entry in security settings""
    This reverts commit d1e53b2861ec0dc5f29ebc19b68dbff6bcf8d20c.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -146,8 +146,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
 
-        // App security settings
-        addPreferencesFromResource(R.xml.security_settings_app_slim);
 
         // Add options for lock/unlock screen
         int resid = 0;
