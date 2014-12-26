1f791b1a9026e3cc9b40b772618fd3ec379ff0e3
Lethargy <Letharqy@gmail.com>
2014-12-27 00:26:28 +1100

    Revert "ふかつどう: Allow Heads Up toggle on system apps (1/2)"
    Derp, FCs. Revert for now and do this later I guess.
    
    This reverts commit 6f18f397d347ac6f8ab192553c3f2da21808d6a3.

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -272,6 +272,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
 
         if (isSystemPkg) {
             prefs.removePreference(mBlock);
+            prefs.removePreference(mHeadsUp);
             mPriority.setDependency(null); // don't have it depend on a preference that's gone
         }
 
